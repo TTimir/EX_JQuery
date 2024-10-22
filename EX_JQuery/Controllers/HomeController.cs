04e9cdc0a85d91592ddd0c709045d5adf7532eab
Timir Bhingradiya <95134530+TTimir@users.noreply.github.com>
2024-10-23 00:25:00 +0530

    Add Ajax

--- EX_JQuery/Controllers/HomeController.cs
+++ EX_JQuery/Controllers/HomeController.cs
@@ -18,6 +18,29 @@ namespace EX_JQuery.Controllers
             return View();
         }
 
+        [HttpPost]
+        public int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        [HttpPost]
+        public int Subtract(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+
+        [HttpPost]
+        public Calculation Calulate(int num1, int num2)
+        {
+            Calculation mod = new Calculation();
+            mod.Add = num1 + num2;
+            mod.Substract = num1 - num2;
+            mod.Multiply = num1 * num2;
+            mod.Division = (decimal) num1 / num2;
+            return mod;
+        }
+
         public IActionResult Privacy()
         {
             return View();
